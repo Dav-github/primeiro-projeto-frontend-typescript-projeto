d66bad40835415abbbcea36bdb197649ba0cde04
crow <d4v3.oliveira@gmail.com>
2024-08-10 00:27:41 -0300

    01-Login-css: done

--- base/src/pages/Home/index.tsx
+++ base/src/pages/Home/index.tsx
@@ -1,7 +1,52 @@
+import { Container } from "../../components/Container";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
+
+const DivForm = styled.div`
+    width: 494px;
+    height: 475px;
+    background-color: var(--cinza-claro);
+    padding: 0px 97px;
+`;
+
 export default function Home() {
     return (
-        <>
-            <h1>HOME</h1>
-        </>
+        <Container
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <DivForm>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginTop: "32px",
+                    }}
+                >
+                    <img src={logo} alt="Logo Cubos Academy" />
+                </div>
+                <form action="">
+                    <input
+                        type="text"
+                        placeholder="E-mail"
+                        style={{ marginTop: "60px" }}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Password"
+                        style={{ marginTop: "16px" }}
+                    />
+                    <p style={{ color: "var(--branco)", marginTop: "16px" }}>
+                        Não tem cadastro? Clique aqui!
+                    </p>
+                    <button type="submit" style={{ marginTop: "60px" }}>
+                        Login
+                    </button>
+                </form>
+            </DivForm>
+        </Container>
     );
 }
